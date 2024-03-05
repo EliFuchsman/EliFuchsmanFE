1dfc521176dcc4e2eb5a424bb49d045d75c9a968
Eli Fuchsman <efuchsman@gmail.com>
2024-03-05 15:08:57 -0700

    Add Redlines

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,20 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Button from '../Button/Button';
+import RedLine from '../Redline/Redline';
 
 function App() {
   const [showGif, setShowGif] = useState(true);
+  const [gifOpacity, setGifOpacity] = useState(1);
+  const [showWelcomeText, setShowWelcomeText] = useState(false);
+  const [textOpacity, setTextOpacity] = useState(0);
 
   useEffect(() => {
-
-    const timeoutId = setTimeout(() => {
-      setShowGif(false);
+    const gifTimeoutId = setTimeout(() => {
+      setGifOpacity(0);
     }, 4000);
-    return () => clearTimeout(timeoutId)
+
+    const welcomeTextTimeoutId = setTimeout(() => {
+      setShowGif(false);
+      setTextOpacity(1);
+      setShowWelcomeText(true);
+    }, 6000);
+    return () => {
+      clearTimeout(gifTimeoutId);
+      clearTimeout(welcomeTextTimeoutId);
+    }
   }, []);
 
   const headerStyle = {
     backgroundColor: showGif ? '#000' : '#282c34',
+  };
+
+  const gifStyle: React.CSSProperties = {
+    opacity: gifOpacity,
+    transition: 'opacity 2s ease',
   };
 
   const buttonContainerStyle: React.CSSProperties = {
@@ -24,18 +41,27 @@ function App() {
     width: '60%',
   };
 
+  const welcomeTextStyle: React.CSSProperties = {
+    opacity: textOpacity,
+    transition: 'opacity 2s ease',
+    color: '#fff',
+  };
+
   return (
     <div className="App" style={headerStyle}>
       <header className="App-header">
-        {showGif ? (
+        {showGif && (
           <img
             src="https://static.demilked.com/wp-content/uploads/2016/06/gif-animations-replace-loading-screen-10.gif"
             className="Loading-gif"
             alt="gif"
+            style={gifStyle}
           />
-        ) : (
+        )}
+        {showWelcomeText && (
           <div>
-            <h1>Welcome to Eli Fuchsman's Portfolio</h1>
+            <RedLine />
+            <h1 style={welcomeTextStyle}>Welcome to Eli Fuchsman's Portfolio</h1>
             <div style={buttonContainerStyle}>
               <Button variation="linkedin" buttonText="Visit LinkedIn" link="https://www.linkedin.com/in/elifuchsman/" />
               <Button variation="github" buttonText="Visit GitHub" link="https://www.github.com/efuchsman/" />
@@ -45,7 +71,7 @@ function App() {
           </div>
         )}
       </header>
-    </div >
+    </div>
   );
 }
 
---
+++ src/Components/Redline/Redline.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Redline.css';
+
+const RedLine: React.FC = () => {
+  return <div className="red-line"></div>;
+};
+
+export default RedLine;
